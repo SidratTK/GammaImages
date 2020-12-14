24da22dd2a483ab3b12a42e82a6c1df0fe1c9f63
SidratTK <sidrattasawoor32@gmail.com>
2020-12-14 19:41:41 +0000

    Create gammaModelPath_st.m

---
+++ gammaModelPath_st.m
@@ -0,0 +1,21 @@
+function rootPath = gammaModelPath_st()
+% Set directory path for Gamma Model project
+%
+%     gammaModelPath
+%
+% Set up the path to the functions called by Gamma Model functions.
+% Put data from OSF in rootPath/data/
+
+rootPath = fileparts(which(mfilename));
+fprintf('gamma model code directory: %s\n',rootPath)
+
+% Adds the root directory of the Gamma Model tree to the user's path
+addpath(rootPath);
+
+% Generates a list of the directories below the Gamma Model tree.
+% addpath(fullfile(rootPath, 'compute'))
+% addpath(fullfile(rootPath, 'data_info'))
+% addpath(fullfile(rootPath, 'render'))
+
+return;
+
