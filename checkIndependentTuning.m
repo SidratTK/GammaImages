09ab2d8848e6d062d0b57e94ed8fbc21689f858f
SidratTK <sidrattasawoor32@gmail.com>
2020-12-27 19:42:16 +0000

    updated to include linear models of svd and marginals approximations. significance tests

--- checkIndependentTuning.m
+++ checkIndependentTuning.m
@@ -6,95 +6,200 @@
 % product of marginals
 % see also Mazer et al., 2002 PNAS who follow a similar marginal analysis
 % the observed joint tuning (data) and estimated separable tuning (prod of
-% marginals) are compared via the Pearson correlation coefficient, the of
-% which represents the fraction of variance in the data shared by the
-% estimate.
+% marginals) are compared via the Pearson correlation coefficient, the square of
+% which represents the fraction of variance in the data shared by the estimate.
 % Note that the product of the marginal tuning may not be the same order of
 % magnitude as the observed data, but the correl coeff is not affected by
-% scaling or shifting. So it is the relative trend in the 2d data that is
-% being compared.
-% STK 091220
-% 101220: updates to normalise data2D so it behaves like a prob distribn.
-% Make sure only +ve values are input
-% added option of plotting marginals - 171220
+% intercept. 
+% also calculate separability index as in Mazer et al.
+% see Pena & Konishi compare additive (marginals) & multiplicative (factors) models
+% use Model fitting to get the separable estimates. The R2 in linear model is
+% v.v.close to vals obtained by data & importantly gives F statistic.
+% REFS: Mazer et al., 2002, PNAS, Pena & Konishi, 2001, Science,
 
+% STK 091220
+% 101220: normalise data2D so it behaves like a prob distribn. Make sure only +ve values are input
+% 171220: added option of plotting marginals 
+% 221220: included SVD. and model estimates
+% 231220: add the coef test to get f stat of models. change the output format
 
 % make dummy data example
-% data2DIn = rand(3,3);        % 2 dimensional data
-% parV = [0:pi/4:pi]; mparV = pi/2; sparV = pi/8; margV1 = pdf('Normal',parV,mparV,sparV); margV1 = margV1(:); 
-% parH = [1:4];     mparH = 2; sparH = 1;    margH1 = pdf('Normal',parH,mparH,sparH);
+% data2DIn = rand(4,3);        % 2 dimensional random data OR
+% parV = [1:8]; mparV = 4; sparV = 2; margV1 = pdf('Normal',parV,mparV,sparV); margV1 = margV1(:); 
+% parH = [1:4]; mparH = 2; sparH = 1; margH1 = pdf('Normal',parH,mparH,sparH);
 % data2DIn = margV1 * margH1;
+% checkIndependentTuning(data2DIn,1)
 
-function [rout,pout,gof,sepindex] = checkIndependentTuning(data2DIn,showfig,labH,labV)
+function [SVDPrdOut,sepindex,MargPrdOut,MargSumOut,MargBthOut,chi2MargPrdOut,mrgsvdcorr] = checkIndependentTuning(data2DIn,showfig,labH,labV)
 if ~exist('showfig','var'), showfig=0;   end
 
+varH   = 1:size(data2DIn,2);
+varV   = 1:size(data2DIn,1);
+if ~exist('labH','var'), labH = varH; end
+if ~exist('labV','var'), labV = varV; end
+
+% get marginal sums of row & col
+sumVa = repmat(sum(data2DIn,2),[1,size(data2DIn,2)]);
+sumHa = repmat(sum(data2DIn,1),[size(data2DIn,1),1]);
+
 % data values tells how likely a chosen input combination is to generate a strong response. 
-% want the data to behave like probabilities, so sum should be 1
-% as the stimulus combinations have been shown independently, it means each
-% element of 2D data is independently sampled. 
-% the 3 conditions of being a pdf are met: +ve, sum=1, independent samples
+% if want the data to behave like probabilities, sum should be 1, so normalise
+% the stimulus combs shown independently, => 2D data is independently sampled. 
+% the 3 conditions of being a pdf are met: +ve, sum=1, independent samples 
+sumis  = sum(sum(data2DIn));
+data2D = data2DIn/sumis;  % subsequent linear corrs hold even if this normalisation not done.
 
-data2D = data2DIn/sum(sum(data2DIn));
-varH   = 1:size(data2D,2);
-varV   = 1:size(data2D,1);
-
-% get marginals
+% 1. get marginals (normalised)
 margV = sum(data2D,2);    % marginal of Vertical param
 margH = sum(data2D,1);    % marginal of Horizontal param
 
-% separable product
-data2DInd = margV*margH;   % column vector x row vector
+% 2. get SVD factors,
+[U,S,V] = svd(data2DIn,'econ');  
+S = diag(S); factH = V(:,1); factV = U(:,1);    % val & first singular vectors
+sepindex = S(1)^2/(sum(S.^2));
 
-% try with GOF, calculates error values
-errtype = 'MSE';
-gof = goodnessOfFit(data2DInd(:),data2D(:),errtype);   % 
+% 3. correlation between the factors
+[rh,ph] = corr(margH', factH);
+[rv,pv] = corr(margV , factV);
 
-% r2 statistic as in Mazer et al 2002 Pnas
-[r,p] = corr([data2DInd(:), data2D(:)]);
-rout = r(2); pout=p(2);
+% 4A. separable product of marginals
+data2DIndp = margV*margH*sumis;     % column vector x row vector
+[r,pr] = corr([data2DIndp(:), data2DIn(:)]);  % Corr coeff statistic as in Mazer et al 2002, for R2
+rdta_margprd = r(2); pdta_margprd = pr(2);
 
-% the SVD route  % also in Mazer et al, similar to Depireux et al. 2001 JNP
-[U,S,V] = svd(data2DIn,'econ');
-singvec=diag(S);
-sepindex = singvec(1)^2/(sum(singvec.^2));
+% 4B. separable sum of marginals
+data2DInds  = sumis*(repmat(margV,[1,size(data2D,2)]) + repmat(margH,[size(data2D,1),1]));  % col val + row val
+[r,pr]      = corr([data2DInds(:), data2DIn(:)]);
+rdta_margsum= r(2); pdta_margsum = pr(2);
 
-% display
+% 4C. separable product of svd factors 
+data2Dsvd = factV * S(1,1) * factH';      
+[r,pr]    = corr([data2Dsvd(:), data2DIn(:)]);  % Correlation coeff for getting R2
+rdta_svdprd = r(2); pdta_svdprd = pr(2);
+
+% 5. calc the chi2 between the dataIn and product of marginals, 
+wid = 'stats:chi2gof:LowCounts';   warning('off',wid);
+bincntrs = 1:length(varH)*length(varV);   %  get bin numbers
+[hchi,pchi,st] = chi2gof(bincntrs,'Ctrs',bincntrs,'Frequency',data2DIn(:),'Expected',data2DIndp(:));   
+warning('on',wid);
+
+% 6A. use model fits for the above marginal model & svd model
+% use data2DIn vs data2D or sumH vs margH doesnt change the signficance but only model coeff magnitudes
+y    = data2DIn(:);
+sumH = sumHa(:);   
+sumV = sumVa(:);
+d    = dataset(y, sumH, sumV);
+LMsum_marg = LinearModel.fit(d,'y ~ sumH + sumV');
+LMprd_marg = LinearModel.fit(d,'y ~ sumH : sumV');
+LMbth_marg = LinearModel.fit(d,'y ~ sumH * sumV');  % just check both interactions
+fH = sqrt(S(1))* repmat(factH(:)',[size(data2DIn,1),1]); fH = fH(:);
+fV = sqrt(S(1))* repmat(factV(:) ,[1,size(data2DIn,2)]); fV = fV(:);
+d2 = dataset(y, fH, fV);
+LMprd_svd = LinearModel.fit(d2,'y ~ fH : fV');
+
+% 6B. pick up some features from these models here:
+r2svdprd = LMprd_svd.Rsquared.Ordinary;   % R2 reps ratio of variance explained by the model
+r2margprd= LMprd_marg.Rsquared.Ordinary;
+r2margsum= LMsum_marg.Rsquared.Ordinary;
+r2margbth= LMbth_marg.Rsquared.Ordinary;
+[p_svdprd, f_svdprd, d_svdprd]  = coefTest(LMprd_svd); % p<palpha - model significantly diff than intercept-only model
+[p_margprd,f_margprd,d_margprd] = coefTest(LMprd_marg); % f = f value from the f test
+[p_margsum,f_margsum,d_margsum] = coefTest(LMsum_marg);
+[p_margbth,f_margbth,d_margbth] = coefTest(LMbth_marg);
+
+% 7. Update the estimates using the models
+data2DIndp = predict(LMprd_marg,[sumH,sumV]); data2DIndp = reshape(data2DIndp,size(data2DIn));
+data2DInds = predict(LMsum_marg,[sumH,sumV]); data2DInds = reshape(data2DInds,size(data2DIn));
+data2Dsvd  = predict(LMprd_svd ,[fH,fV]);     data2Dsvd  = reshape(data2Dsvd ,size(data2DIn));
+
+% 8. display
 if showfig
     figH = figure;
     figH.Units = 'centimeters';
     figH.PaperType  = 'a4';
     figH.PaperUnits = 'centimeters';
-    figH.PaperSize  = [17.5 10]; % figH.PaperSize  = [17.8 25.1];
+    figH.PaperSize  = [17.5 15];
     figH.PaperOrientation = 'Portrait';
     figH.PaperPosition = [0 0 figH.PaperSize];
     figH.Color = [1 1 1];
     figH.Position = [0 0 figH.PaperSize];
     figH.PaperUnits = 'normalized';
     
-    plot2D = getPlotHandles(1,1,[0.25 0.15 0.3 0.3], 0.02,0.02);
-    plot2DIn = getPlotHandles(1,1,[0.65 0.15 0.3 0.3], 0.02,0.02);    
-    plotMh = getPlotHandles(1,1,[0.25 0.65 0.3 0.15], 0.02,0.02);
-    plotMv = getPlotHandles(1,1,[0.075 0.15 0.1 0.3], 0.02,0.02);
+    plotMh = getPlotHandles(1,1,[0.22 0.80 0.30 0.15], 0.02,0.02);
+    plots1 = getPlotHandles(1,1,[0.61 0.80 0.30 0.15], 0.02,0.02);
+    plotMv = getPlotHandles(1,1,[0.01 0.45 0.10 0.25], 0.02,0.02);
+    plot2D = getPlotHandles(1,1,[0.22 0.45 0.30 0.25], 0.02,0.02);
+    plot2Dsum = getPlotHandles(1,1,[0.22 0.10 0.30 0.25], 0.02,0.02);
+    plot2Dprd = getPlotHandles(1,1,[0.61 0.10 0.30 0.25], 0.02,0.02);    
+    plot2Dsvd = getPlotHandles(1,1,[0.61 0.45 0.30 0.25], 0.02,0.02);
     
-    axes(plot2D); imagesc(data2D);
-    title(plot2D,'2D tuning');
+    cols = gray(10);
+    hold(plotMh,'on');  hold(plotMv,'on'); camroll(plotMv,90);
+%     t = plot(plotMh,varH,data2DIn./sumVa,'LineWidth',1,'Linestyle',':');
+%     set(t,{'color'},num2cell(cols(1:length(varV),:),2));
+%     t = plot(plotMv,varV,data2DIn./sumHa,'LineWidth',1,'Linestyle',':');
+%     set(t,{'color'},num2cell(cols(1:length(varH),:),2));
     
-    axes(plot2DIn); imagesc(data2DInd);
-    title(plot2DIn,'2D separable tuning');
+    bar(plots1,[1:length(S)],100*(sqrt(S.^2)./sum(sqrt(S.^2))));   % magnitude of sing vectors
     
-    plot(plotMh,varH,margH,'LineWidth',2);
-    plot(plotMv,varV,margV,'LineWidth',2); camroll(plotMv,90);
+    axes(plot2D); imagesc(flip(data2DIn,1));
+    title(plot2D,'2D Tuning Data Y');
     
-    set(plot2D,'XTick',varH,'XTickLabel',labH,'YTick',varV,'YTickLabel',labV,'tickdir','out','box','off');
-    set(plot2DIn,'XTick',varH,'XTickLabel',labH,'YTick',varV,'YTickLabel',labV,'tickdir','out','box','off')
-    set(plotMh,'XTick',varH,'XTickLabel',labH,'tickdir','out','box','off');
-    set(plotMv,'XTick',varV,'XTickLabel',labV,'tickdir','out','box','off');
+    axes(plot2Dprd); imagesc(flip(data2DIndp,1));
+    title(plot2Dprd,'Y ~ 1 + F * G');
     
+    axes(plot2Dsum); imagesc(flip((data2DInds),1));
+    title(plot2Dsum,'Y ~ 1 + F + G');
     
+    axes(plot2Dsvd); imagesc(flip(data2Dsvd,1));
+    title(plot2Dsvd,'Y ~ 1 + U(1)*s1*V(1)');
+    
+    plot(plotMh,varH,margH,'LineWidth',2,'Color','b'); 
+    plot(plotMv,varV,margV,'LineWidth',2,'Color','b'); 
+    
+    plot(plotMh,varH,factH/sum(factH),'LineWidth',2,'Color','r','Linestyle','--');
+    plot(plotMv,varV,factV/sum(factV),'LineWidth',2,'Color','r','Linestyle','--'); 
+    
+    set(plot2D,'XTick',varH,'XTickLabel',labH,'YTick',varV,'YTickLabel',flip(labV),'XTickLabelRotation',0,'tickdir','out','box','off','fontWeight','bold');
+    set(plot2Dprd,'XTick',varH,'XTickLabel',labH,'YTick',varV,'YTickLabel',flip(labV),'XTickLabelRotation',0,'tickdir','out','box','off','fontWeight','bold')
+    set(plot2Dsum,'XTick',varH,'XTickLabel',labH,'YTick',varV,'YTickLabel',flip(labV),'XTickLabelRotation',0,'tickdir','out','box','off','fontWeight','bold')
+    set(plot2Dsvd,'XTick',varH,'XTickLabel',labH,'YTick',varV,'YTickLabel',flip(labV),'XTickLabelRotation',0,'tickdir','out','box','off','fontWeight','bold')
+    
+    set(plotMh,'XTick',varH,'XTickLabel',labH,'tickdir','out','box','off','fontWeight','bold');
+    set(plotMv,'XTick',varV,'XTickLabel',labV,'tickdir','out','box','off','fontWeight','bold');  
+    
+    set(plots1, 'box','off');
+    
+    text(1.01,0.9, ['R2=',num2str(round(r2margprd,2))],'Parent',plot2Dprd,'Units','normalized','FontSize',10);
+    text(1.01,0.9, ['R2=',num2str(round(r2svdprd,2)) ],'Parent',plot2Dsvd,'Units','normalized','FontSize',10);
+    text(-0.4,0.9, ['R2=',num2str(round(r2margsum,2))],'Parent',plot2Dsum,'Units','normalized','FontSize',10);
+%     text(1.01,0.8, 'Ftest','Parent',plot2Dprd,'Units','normalized','FontSize',10);
+%     text(1.01,0.8, 'Ftest','Parent',plot2Dsvd,'Units','normalized','FontSize',10);
+%     text(-0.1,0.8, 'Ftest','Parent',plot2Dsum,'Units','normalized','FontSize',10);
+%     text(1.01,0.7, ['p =',num2str(round(p_margprd,2))],'Parent',plot2Dprd,'Units','normalized','FontSize',10);
+%     text(1.01,0.7, ['p =',num2str(round(p_svdprd,2))], 'Parent',plot2Dsvd,'Units','normalized','FontSize',10);
+%     text(-0.1,0.7, ['p =',num2str(round(p_margsum,2))],'Parent',plot2Dsum,'Units','normalized','FontSize',10);
+
     printf('\n');
-    disp(['Variance explained by independent model is ',num2str(100*(r(2).^2)),'% ']);
+    disp(['Variance explained by product of marginals is ',num2str(100*(rdta_margprd.^2)),'% ']);
+    disp(['Variance explained by model fit of marginals product is ',num2str(100*r2margprd),'% .',...
+        ' model F-val= ',num2str(round(f_margprd,2)),' p= ',num2str(round(p_margprd,3))]);
+    disp(['Chi2 test for difference bw observed & independent model data is h = ',num2str(hchi)]);
+    disp(['Variance explained by sum of marginals is ',num2str(100*(rdta_margsum.^2)),'% ']);
+    disp(['Variance explained by model fit of marginals sum is ',num2str(100*r2margsum),'% .',...
+        ' model F-val= ',num2str(round(f_margsum,2)),' p= ',num2str(round(p_margsum,3))]);
     disp(['Separability index from SVD factors is ',num2str(sepindex)]);
-    disp([errtype,' GoodnessOfFit of independent model is ',num2str(gof)]);
+    disp(['Variance explained by product of 1st SVD vectors is ',num2str(100*(rdta_svdprd.^2)),'% ']);
+    disp(['Variance explained by model fit of svd product is ',num2str(100*r2svdprd),'% .',...
+        ' model F-val= ',num2str(round(f_svdprd,2)),' p= ',num2str(round(p_svdprd,3))]);
 end
+
+% 9. compile outputs
+chi2MargPrdOut = [hchi,pchi,st.chi2stat];
+MargPrdOut.Mdl = LMprd_marg; MargPrdOut.r2=r2margprd; MargPrdOut.fstat=[f_margprd,p_margprd,d_margprd];
+MargSumOut.Mdl = LMsum_marg; MargSumOut.r2=r2margsum; MargSumOut.fstat=[f_margsum,p_margsum,d_margsum];
+MargBthOut.Mdl = LMbth_marg; MargBthOut.r2=r2margbth; MargBthOut.fstat=[f_margbth,p_margbth,d_margbth];
+SVDPrdOut.Mdl  = LMprd_svd;  SVDPrdOut.r2 =r2svdprd;  SVDPrdOut.fstat =[f_svdprd, p_svdprd, d_svdprd ];
+mrgsvdcorr     = [rh rv ph pv];
 
 end
--- checkIndependentTuningEnv.m
+++ checkIndependentTuningEnv.m
@@ -28,7 +28,6 @@ for sub = 1:2
     subject = subjectNames{sub};
     [expDates, protocolNames, protocolTypes, electrodes_sz, electrodeTypes] = getProtocolInfoGratings(subject);
     psz = strcmp(protocolTypes,'SizeOri');
-    
     namedetail1 = [num2str(fBandGamma(1)),'_',num2str(fBandGamma(2)),'_tST_',num2str(tST(1)*1000),'_',num2str(tST(2)*1000),'_Tapers',num2str(numTapers),'_',subject,'_elec'];
     namedetail2 = ['_method',num2str(PwrmethodFlag),'_notch',num2str(useNotchData)];
     
@@ -39,56 +38,100 @@ for sub = 1:2
         else                                 % else use all good elecs.
             electrodes = cat(1, EcogElectrodeList{sub}, LFPElectrodeList{sub});   
         end 
+        
         for e = 1:length(electrodes)
             elecis = electrodes(e);
+            pName =  ''; eDate =  '';
             if strcmp(protocolTypes{pt},'SizeOri') || elecis>81 
                 indel = find(electrodes_sz==elecis);
-                pName =  protocolNames{psz}{indel}; eDate =  expDates{psz}{indel};
-            else
-                pName =  ''; eDate =  '';
+                pName = protocolNames{psz}{indel}; eDate =  expDates{psz}{indel};
             end
             namedetail = ['dGamma_',namedetail1,num2str(elecis),'_',protocolTypes{1},'_',protocolTypes{2},eDate,pName,'_',protocolTypes{3},namedetail2,'.mat'];
             fname = fullfile(dataDir,'derivatives','dGamma',namedetail);
             dG    = load(fname);
             delGuse = squeeze(dG.delGamma{pt});
-            [r,ptemp,g,sepind] = checkIndependentTuning(delGuse);
+            
+            varH = dG.parameterCombinations{pt}.oValsUnique;
+            if strcmp(protocolTypes{pt},'SizeOri'),
+                varV = dG.parameterCombinations{pt}.sValsUnique;
+            elseif  strcmp(protocolTypes{pt},'SFOri')
+                varV = dG.parameterCombinations{pt}.fValsUnique;
+            elseif  strcmp(protocolTypes{pt},'ConOri')
+                varV = dG.parameterCombinations{pt}.cValsUnique;
+            end 
+            
             if strcmp(subject,'alpaH') && elecis==41     % display for typical elec
-                varH = dG.parameterCombinations{pt}.oValsUnique;
-                if strcmp(protocolTypes{pt},'SizeOri'),
-                    varV = dG.parameterCombinations{pt}.sValsUnique;
-                elseif strcmp(protocolTypes{pt},'SFOri')
-                    varV = dG.parameterCombinations{pt}.fValsUnique;
-                elseif strcmp(protocolTypes{pt},'ConOri')
-                    varV = dG.parameterCombinations{pt}.cValsUnique;
-                end
-                [r,ptemp,g,sepind] = checkIndependentTuning(delGuse,1,varH,varV);
+                showflg = 1;
+            else
+                showflg = 0;
             end
-            rsq{sub,pt}(e) = r^2;
-            p{sub,pt}(e)   = ptemp; 
-            gof{sub,pt}(e) = g;
+            
+            [SVDPrd{sub,pt}{e},sepind,MrgPrd{sub,pt}{e},MrgSum{sub,pt}{e},MrgBth{sub,pt}{e},chi2MrgPrd,mrgsvdcorr] ...
+                = checkIndependentTuning(delGuse,showflg,varH,varV);
+            
             sep{sub,pt}(e) = sepind;
+            hchi{sub,pt}(e)= chi2MrgPrd(1);
+            rmguv{sub,pt}(e,:)= mrgsvdcorr;
+            
         end 
     end
 end
 printf('\n');
 printf('Squared correlation R2 represents how much variance is shared between 2 distributions'); 
 printf('Or how much variance of A(observed-data) can be explained by B(separable-tuning-estimate)')
-printf('GOF is a goodness of fit gives the error metric between the two distributions like MSE');
 printf('Separability index is calculated from SVD decomposition and tells us how well the data can be factored into independent components');
+printf('h-value of chi2test tests whether the observed values could have been drawn from the expected distribution');
 
 palph = 0.05;
 for sub = 1:2
     printf(subjectNames{sub});
     for pt=1:3
-        r2 = round(mean(rsq{sub,pt}),2);    sr = round(std(rsq{sub,pt}),2);
-        n  = length(rsq{sub,pt});
-        s  = sum(p{sub,pt} <= palph);
-        g  = round(mean(gof{sub,pt}),2);    sg = round(std(gof{sub,pt}),2);
+        
         sepind = round(mean(sep{sub,pt}),2);ss = round(std(sep{sub,pt}),2);
+        his    = sum(hchi{sub,pt} == 0);
+        
+        % FOR NOW DISPLAYING ALL :: 
+        r = cell2mat(cellfun(@(x) x.r2, SVDPrd{sub,pt},'un',0));
+        r2svd = round(mean(r),2); sr2svd = round(std(r),2);
+        n     = length(r);
+        pfsvd = cell2mat(cellfun(@(x) x.fstat(2), SVDPrd{sub,pt},'un',0));
+        pfsvd = sum(pfsvd <= palph);
+        
+        r1 = cell2mat(cellfun(@(x) x.r2, MrgPrd{sub,pt},'un',0));
+        r2prd = round(mean(r1),2); sr2prd = round(std(r1),2);
+        pfprd = cell2mat(cellfun(@(x) x.fstat(2), MrgPrd{sub,pt},'un',0));
+        pfprd = sum(pfprd <= palph);
+        
+        r2 = cell2mat(cellfun(@(x) x.r2, MrgSum{sub,pt},'un',0));
+        r2sum = round(mean(r2),2); sr2sum = round(std(r2),2);
+        pfsum = cell2mat(cellfun(@(x) x.fstat(2), MrgSum{sub,pt},'un',0));
+        pfsum = sum(pfsum <= palph);
+        
+        r3 = cell2mat(cellfun(@(x) x.r2, MrgBth{sub,pt},'un',0));
+        r2bth = round(mean(r3),2); sr2bth = round(std(r3),2);
+        pfbth = cell2mat(cellfun(@(x) x.fstat(2), MrgBth{sub,pt},'un',0));
+        pfbth = sum(pfbth <= palph);
         
         disp([' ', protocolTypes{pt},':',', n = ',num2str(n)]);
-        printf(['\t r2 = ',num2str(r2),'+-',num2str(sr), 'SD. Significant p-val for ',num2str(s),' of ',num2str(n)]);
-        printf(['\t GOF = ',num2str(g),'+-',num2str(sg), 'SD.\n\t Separability index = ',num2str(sepind),'+-',num2str(ss),'SD']);
+        printf(['\t r2 = ',num2str(r2svd),'+-',num2str(sr2svd), 'SD. Significant F-stat p-val for ',num2str(pfsvd),' of ',num2str(n)]);
+        printf(['\t Separability index (svd) = ',num2str(sepind),'+-',num2str(ss),'SD']);
+        
+        printf(['\t r2 = ',num2str(r2prd),'+-',num2str(sr2prd), 'SD. Significant F-stat p-val for ',num2str(pfprd),' of ',num2str(n)]);
+        printf(['\t h = 0 (fail to reject same distribution) in , ', num2str(his),' of ',num2str(n)]);
+    
+        printf(['\t r2 = ',num2str(r2sum),'+-',num2str(sr2sum), 'SD. Significant F-stat p-val for ',num2str(pfsum),' of ',num2str(n)]);
+
+        printf(['\t r2 = ',num2str(r2bth),'+-',num2str(sr2bth), 'SD. Significant F-stat p-val for ',num2str(pfbth),' of ',num2str(n)]);
+
+        [h,p] = ttest(r2,r1); % t-test for whether R2 of MrgPrd is significantly diff from MrgSum
+        printf(['\t paired ttest on R2 of Marg Sum & Prd. H = ',num2str(h),'P = ',num2str(round(p,3))]);
+        
+        inds = (rmguv{sub,pt}(:,3)<palph) & (rmguv{sub,pt}(:,4)<palph);
+        mr = mean(abs([rmguv{sub,pt}(inds,1);rmguv{sub,pt}(inds,2)]));
+        sr = std(abs([rmguv{sub,pt}(inds,1);rmguv{sub,pt}(inds,2)]));
+        printf(['\t Corr bw SVD factors and marginals was significant for ',num2str(sum(inds)),' of ',num2str(n)]);
+        printf(['\t Avg mag of Corr coef =', num2str(round(mr,2)),' +-',num2str(round(sr,2)),' SD']);
+        
     end
 end
 
